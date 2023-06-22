bb64dc569f8635817d00b43c14e288a15fae5dc0
Sara Al-Assam <sarassam@gmail.com>
2023-06-22 12:48:57 +0100

    Add PolyaUrn DPMM and use instead of Walker

---
+++ src/PolyaUrnDPMM.h
@@ -0,0 +1,26 @@
+#ifndef CARBONDATE_POLYAURNDPMM_H
+#define CARBONDATE_POLYAURNDPMM_H
+#include "DPMM.h"
+
+class PolyaUrnDPMM : public DPMM  {
+private:
+    // Instant values of DPMM parameters
+    std::vector<int> observations_per_cluster_i;
+
+    // Stored values of DPMM parameters
+    std::vector<std::vector<int>> observations_per_cluster;
+
+private:
+    void initialise_storage() override;
+    void initialise_clusters() override;
+    void perform_update_step() override;
+    void store_current_values(int i) override;
+    void update_cluster_ids();
+    void create_new_phi_and_tau(double calendar_age, double &phi, double &tau);
+    void update_phi_and_tau();
+    double alpha_log_likelihood(double alpha_value) override;
+    double calculate_density_sample(int sample_id, double calendar_age) override;
+};
+
+
+#endif //CARBONDATE_POLYAURNDPMM_H
