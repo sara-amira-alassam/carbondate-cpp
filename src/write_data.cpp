0c3f59f4c07eb0ad2f3eb95e37708ddb0eb728ae
Sara Al-Assam <sarassam@gmail.com>
2024-01-24 12:14:52 +0000

    Some tidying up

--- src/write_data.cpp
+++ src/write_data.cpp
@@ -2,23 +2,31 @@
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 #include <fstream>
-#include "log.h"
+#include "write_data.h"
+
 
 std::string log_file_path() {
     return project_directory + project_name + ".log";
 }
 
-/*
- * Must be called before any calls to update the log file. Checks it exists and adds the carbondate reference.
- * Note there must first be a call to read_arguments() to set the prefix for the file.
- */
+
+std::string text_file_path() {
+    return project_directory + project_name + ".txt";
+}
+
+
+std::string js_output_file_path() {
+    return project_directory + project_name + ".js";
+}
+
+
 void initialize_log_file() {
     std::string filepath = log_file_path();
 
     update_log_file(carbondate_full_reference());
 }
 
-/* Adds line to the log file. Note initialize_log_file() must be called once before any call to update the log file */
+
 void update_log_file(const std::string& log_line) {
     std::string filepath = log_file_path();
 
@@ -37,6 +45,30 @@ void update_log_file(const std::vector<std::string>& log_lines) {
     if (!file.is_open()) throw UnableToWriteToLogFileException(filepath);
 
     for (const std::string &log_line : log_lines)  file << log_line << std::endl;
+
+    file.close();
+}
+
+
+void update_text_file(const std::vector<std::string>& text_lines) {
+    std::string filepath = text_file_path();
+
+    std::ofstream file(filepath, std::fstream::app);
+    if (!file.is_open()) throw UnableToWriteToTextFileException(filepath);
+
+    for (const std::string &text_line : text_lines)  file << text_line << std::endl;
+
+    file.close();
+}
+
+
+void update_js_output_file(const std::vector<std::string>& output_lines) {
+    std::string filepath = js_output_file_path();
+
+    std::ofstream file(filepath, std::fstream::app);
+    if (!file.is_open()) throw UnableToWriteToOutputFileException(filepath);
+
+    for (const std::string &output_line : output_lines)  file << output_line;
 
     file.close();
 }
